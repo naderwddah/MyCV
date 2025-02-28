dd5a7f001731a7790cb6a34dbc89fcb454f171e1
naderwddah <143351057+naderwddah@users.noreply.github.com>
2025-02-28 08:40:53 -0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const words = [' مطور ويب  ' , ' مهندس شبكات ' ,'مطور انظمة ','مهندس برمجيات ','مهندس الإمن السيبراني'];
+let i = 0;
+let txt = "";
+let isDeleting = false;
+const typingSpeed = 120;
+const deletingSpeed = 90;
+const delayBetweenWords = 1500;
+const wrapElement = document.querySelector(".wrap");
+function type() {
+  const fullTxt = words[i % words.length];
+  if (isDeleting) {
+    txt = fullTxt.substring(0, txt.length - 1);
+  } else {
+    txt = fullTxt.substring(0, txt.length + 1);
+  }
+  wrapElement.textContent = txt;
+  let speed = isDeleting ? deletingSpeed : typingSpeed;
+  if (!isDeleting && txt === fullTxt) {
+    speed = delayBetweenWords;
+    isDeleting = true;
+  } else if (isDeleting && txt === "") {
+    isDeleting = false;
+    i++;
+    speed = 500;
+  }
+  setTimeout(() => {
+    requestAnimationFrame(type);
+  }, speed);
+}
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(type, 1e3);
+});
+handAnimation = document.querySelector(".hand");
+btnHire = document.querySelector(".hire");
+setTimeout(() => {
+  handAnimation.style.opacity = 1;
+  btnHire.classList.add("effect");
+}, 8e3);
+setTimeout(() => {
+  handAnimation.style.opacity = 0;
+  btnHire.classList.remove("effect");
+}, 14500);
